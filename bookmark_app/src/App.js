6df3bfca9984673bada70ec13a11c62c96b23b0e
matth3wn <matth3wn@gmail.com>
2019-02-28 09:12:15 -0800

    made some components

--- bookmark_app/src/App.js
+++ bookmark_app/src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import Edit from './components/Edit';
 import './App.css';
 
 class App extends Component {
 
-  componentDidMount(){
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bookmark: []
+    }
+  }
+  componentDidMount() {
     const BASE_URL = 'http://localhost:8000/bookmarks'
     fetch(BASE_URL)
       .then(res => {
-        if(!res.ok)
+        if (!res.ok)
           throw new Error(res.message)
         return res.json();
       })
-      .then(data => console.log(data))
+      .then(data => this.setState({ bookmark: data }))
       .catch(err => console.error(err.message))
   }
+
   render() {
+    let results = this.state.bookmark.map(i => {
+      return(
+        <div key={i.id}>
+        <Link to={`/bookmarks/${i.id}`}><h3>{i.title}</h3></Link>
+        <p>{i.url}</p>
+        <p>{i.description}</p>
+        <p>{i.rating}</p>
+        </div>
+      )
+    })
+
+
     return (
       <div className="App">
-       Test 2
+          <Route exact path='/bookmarks' render={() =>results} />
+          <Route exact path='/bookmarks/:id' component={() => <Edit />} />
       </div>
     );
   }
---
+++ bookmark_app/src/components/Edit.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Edit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div> hello</div>
+    );
+  }
+}
+
+export default Edit;
